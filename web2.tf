20c92d3b098945323e0970fe3a820c4564ec40be
KevinHosey <kevin@mimi.io>
2019-09-29 19:13:55 +0200

    first examples

---
+++ web2.tf
@@ -0,0 +1,24 @@
+resource "digitalocean_droplet" "web2" {
+  image = "ubuntu-16-04-x64"
+  name = "web2"
+  region = "nyc3"
+  size = "1gb"
+  private_networking = true
+  ssh_keys = [
+    "${var.ssh_fingerprint}"
+  ]
+  connection {
+    user = "root"
+    type = "ssh"
+    host = "${digitalocean_droplet.web2.ipv4_address}"
+    private_key = "${file(var.pvt_key)}"
+    timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=PATH:/usr/bin",
+      "sudo apt-get update",
+      "sudo apt-get -y install nginx"
+    ]
+  }
+}
