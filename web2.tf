62119c248e40a185ed11f9c9d922783569d258d4
KevinHosey <kevin@mimi.io>
2019-10-03 14:30:06 +0200

    update web1 and web2

--- web2.tf
+++ web2.tf
@@ -4,21 +4,14 @@ resource "digitalocean_droplet" "web2" {
   region = "fra1"
   size = "1gb"
   private_networking = true
+  user_data = "${file("config/webuserdata.sh")}"
   ssh_keys = [
     "${var.ssh_fingerprint}"
   ]
   connection {
     user = "root"
     type = "ssh"
-    host = "${digitalocean_droplet.web2.ipv4_address}"
     private_key = "${file(var.pvt_key)}"
     timeout = "2m"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=PATH:/usr/bin",
-      "sudo apt-get update",
-      "sudo apt-get -y install nginx"
-    ]
   }
 }
